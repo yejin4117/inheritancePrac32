07e3a414a26b176973a8b1ce48bbf401e1043ad7
Ye-jin Lee <s2108@e-mirim.hs.kr>
2023-06-08 09:13:33 +0900

    강아지, 고양이 개별 클래스 구현

---
+++ inheritancePrac32/Cat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace InheritancePrac32
+{
+    /// <summary>
+    /// 고양이 클래스
+    /// </summary>
+    public class Cat
+    {
+        /// <summary>
+        /// 고양이 나이
+        /// </summary>
+        public int Age { get; set; }
+
+        public void Eat()
+        {
+            Console.WriteLine("냠냠 먹습니다.");
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine("냥냥 웁니다.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("쿨쿨 잠을 잡니다.");
+        }
+    }
+}
--- inheritancePrac32/Program.cs
+++ inheritancePrac32/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace InheritancePrac32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dog> Dogs = new List<Dog>() { new Dog(), new Dog(), new Dog() };
+            List<Cat> Cats = new List<Cat>() { new Cat(), new Cat(), new Cat() };
+
+            foreach (var item in Dogs)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Bark();
+            }
+            foreach (var item in Cats)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Meow();
+            }
+
+
+        }
+    }
+}
