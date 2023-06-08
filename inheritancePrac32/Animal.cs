30ae9bde149965c8ba2e0a6d1b8201a483773d13
Ye-jin Lee <s2108@e-mirim.hs.kr>
2023-06-08 09:19:29 +0900

    상속을 사용하여 중복 코드 최소화

---
+++ inheritancePrac32/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace InheritancePrac32
+{
+    public class Animal
+    {
+        /// <summary>
+        /// 나이
+        /// </summary>
+        public int Age { get; set; }
+
+        Animal() { this.Age = 0; }
+
+        public void Eat()
+        {
+            Console.WriteLine("냠냠 먹습니다.");
+        }
+
+        public void Sleep()
+
+        {
+            Console.WriteLine("쿨쿨 잠을 잡니다.");
+        }
+    }
+}
