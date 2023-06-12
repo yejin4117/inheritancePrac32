4ae3633adbd2dee5157f000959fa4a15c07a3248
Ye-jin Lee <s2108@e-mirim.hs.kr>
2023-06-12 11:51:30 +0900

    상속관계에서의 생성자

--- inheritancePrac32/Animal.cs
+++ inheritancePrac32/Animal.cs
@@ -10,6 +10,11 @@ namespace InheritancePrac32
 
         public Animal() { this.Age = 0; }
 
+        public Animal(int age)
+        {
+            this.Age = age;
+        }
+
         public void Eat()
         {
             Console.WriteLine("냠냠 먹습니다.");
--- inheritancePrac32/Cat.cs
+++ inheritancePrac32/Cat.cs
@@ -7,7 +7,13 @@ namespace InheritancePrac32
     /// </summary>
     public class Cat : Animal
     {
-        
+        public Cat()
+        {
+        }
+        public Cat(int age) : base(age)
+        {
+        }
+
         public void Meow()
         {
             Console.WriteLine("냥냥 웁니다.");
