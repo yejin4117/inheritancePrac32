d42d27655bba3fecf5f17da920f01605767f1067
Ye-jin Lee <s2108@e-mirim.hs.kr>
2023-06-12 11:53:15 +0900

    abstract 키워드 - 객체 생성 방지

--- inheritancePrac32/Cat.cs
+++ inheritancePrac32/Cat.cs
@@ -5,7 +5,7 @@ namespace InheritancePrac32
     /// <summary>
     /// 고양이 클래스
     /// </summary>
-    public class Cat : Animal
+    public abstract class Cat : Animal
     {
         public Cat()
         {
